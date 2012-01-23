83027f7d1d97c12b39ac407bb7688100114e005b
Hannes Matuschek <hmatuschek@gmail.com>
2012-01-23 21:25:45 +0100

    Added missing files.

---
+++ cmake/FindLAPACK.cmake
@@ -0,0 +1,17 @@
+#
+# Try to find LAPACK libraries
+#
+
+find_library(LAPACK_LIBRARY
+             NAMES lapack)
+
+find_library(BLAS_LIBRARY
+             NAMES lapack)
+
+set(LAPACK_LIBRARIES ${LAPACK_LIBRARY} ${BLAS_LIBRARY})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LAPACK  DEFAULT_MSG
+                                  LAPACK_LIBRARY BLAS_LIBRARY)
+
+mark_as_advanced(LAPACK_LIBRARY)
